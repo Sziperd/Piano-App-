3ef68f858e388d7f098b787efe2e5886b37d2d10
Patryk Piwowarczyk <shepherd90@windowslive.com>
2022-02-18 13:45:13 +0100

    Initial Commit

---
+++ Piano_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piano_App
+//
+//  Created by Patryk Piwowarczyk on 18/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
